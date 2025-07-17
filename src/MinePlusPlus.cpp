d87400b381c08a9b9b906e5b87422034421103aa
Lucas Vocanson <lucas.vocanson@gmail.com>
2025-07-17 17:55:02 +0200

    Get SFML

--- src/MinePlusPlus.cpp
+++ src/MinePlusPlus.cpp
@@ -1,9 +1,21 @@
-﻿#include <iostream>
-
-using namespace std;
+﻿#include <SFML/Graphics.hpp>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    auto window = sf::RenderWindow(sf::VideoMode({900u, 600u}), "Mine++");
+    window.setVerticalSyncEnabled(true);
+
+    while (window.isOpen())
+    {
+        while (const auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
 }
